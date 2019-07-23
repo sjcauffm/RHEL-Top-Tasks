3e0bc8fa08cccfdb233b1f261e72226d7a7bcc68
sjcauffm <sjcauffm@ncsu.edu>
2019-07-23 15:11:19 -0400

    Add files via upload

---
+++ top_tasks_reddit_cleaning.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(googlesheets)
+
+dir <- setwd("/Volumes/GoogleDrive/My Drive/UXD-Share/Usability and User Research/Studies 2019/RHEL Top Tasks/Phase 4 - Identifying Top Tasks/Post Summit 2019/Red Hat SubReddit/RHEL-Top-Tasks")
+
+##importing data 
+gs_auth()
+import <- gs_title("rhel_top_tasks_reddit")
+raw_data <- gs_read_csv(import)
+
+#### Adapting code from previous script. Survey structure is slightly different from previous surveys
