d91fb6d4cb6d2c0f70f711f739427340fc6d9ed2
Steve Cauffman <sjcauffm@ncsu.edu>
2019-08-29 10:25:04 -0400

    trying to retool the functions to work with reddit data.

--- top_tasks_reddit_cleaning.R
+++ top_tasks_reddit_cleaning.R
@@ -10,3 +10,35 @@ raw_data <- gs_read_csv(import)
 
 #### Adapting code from previous script. Survey structure is slightly different from previous surveys
 data <- raw_data[3:47,]
+data_trim <- data[,c(9, 18:484)]
+
+names(data_trim)[1] <- c("response_id")
+
+diff <- 
+
+area_names <- c("security_", "storage_", "network_", "virtual_", "identity_", 
+                "entitle_","sysm_", "errata_pick", "migrate_")
+
+top_tasks_by_section <- function (area){
+  area_grep <- grep(area, names(data_trim))
+  area_data <- data_trim[,c(1, area_grep)]
+  area <- gather(area_data, type, value, -response_id)
+}
+
+top_tasks_by_section_split <- function (area){
+  tasks <- data.frame(response_id = 0, type = 0, value = 0)
+  ranks <- data.frame(response_id = 0, type = 0, value = 0)
+  temp <- grep("TEXT", area$type)
+  ranks <- rbind(ranks, area[temp,])
+  ranks <- ranks[complete.cases(ranks$value),]
+  tasks <- rbind(tasks, area[-temp,])
+  tasks <- tasks[complete.cases(tasks$value),]
+  combine <- cbind(tasks, ranks)
+  combine <- combine[which(combine$type != 0), ]
+  combine <- combine[, c(1:3,6)]
+}
+
+unclean <- lapply(area_names, top_tasks_by_section)
+clean <- do.call(rbind.data.frame, unclean)
+names(clean) <- c("response_id", "question", "pick5", "rank5")
+clean_data <- clean
